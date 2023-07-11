2eb28f5b00760c435185c093064193e5583a4184
Tova Holmes <trholmes@login.snowmass21.io>
2023-07-11 12:46:33 -0500

    Small update

--- makeMuonPlots_k4hep.py
+++ makeMuonPlots_k4hep.py
@@ -1,8 +1,12 @@
 import glob
 import ROOT
 
+# Set up some options
+max_events = 1
+
 # Open the edm4hep files with ROOT
 fnames = glob.glob("/data/fmeloni/DataMuC_MuColl_v1/muonGun/edm4hep/*")
+#fnames = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/photonGun_1000/reco_k4/*")
 t = ROOT.TChain("events")
 for f in fnames: t.Add(f)
 
@@ -10,7 +14,18 @@ for f in fnames: t.Add(f)
 n_events = t.GetEntries()
 print("Found %i events."%n_events)
 
+#t.Draw("MCParticle.PDG")
+#input("...")
 
+# Can successfully Draw() but running into issues when doing any kind of loop. Not sure of the cause.
+# Loop over events
+#for e in t: print("!")
 
+'''
+for i, event in enumerate(t):
+    print(i)
+    #if max_events > 0 and i >= max_events: break
+    #if i%100 == 0: print("Processing event %i."%i)
+'''
 
 
