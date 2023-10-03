afe5415f163ad31ccf00e844720907fc613ffb09
Tova Holmes <tova.r.holmes@gmail.com>
2023-10-03 11:05:29 -0500

    Got a working version with the key4hep files, but memory usage is too large.

--- electronStudies.py
+++ electronStudies.py
@@ -14,7 +14,7 @@ exec(open("./plotHelper.py").read())
 ROOT.gROOT.SetBatch()
 
 # Set up some options
-max_events = 10
+max_events = -1
 
 # Open the edm4hep files with ROOT
 samples = glob.glob("/data/fmeloni/DataMuC_MuColl10_v0A/k4reco/electronGun*")
@@ -28,8 +28,15 @@ hists = {}
 for s in files:
     hists[s] = {}
     for obj in ["pfo", "pfo_el", "mcp", "mcp_el", "mcp_el_match"]:
-        for var in variables["obj"]:
-            hists[s][obj+"_"+var] = ROOT.TH1F(s+"_"+obj+"_"+var, s, variables["obj"][var]["nbins"], variables["obj"][var]["xmin"], variables["obj"][var]["xmax"])
+        for vtype in ["obj", "evt"]:
+            for var in variables[vtype]:
+                hists[s][obj+"_"+var] = ROOT.TH1F(s+"_"+obj+"_"+var, s, variables[vtype][var]["nbins"], variables[vtype][var]["xmin"], variables[vtype][var]["xmax"])
+
+# Perform matching between two TLVs
+def isMatched(tlv1, tlv2):
+    if tlv1.DeltaR(tlv2) < 0.1 and abs(tlv1.Perp()-tlv2.Perp())/tlv2.Perp() < 0.2:
+        return True
+    return False
 
 # Loop over the different samples
 for s in files:
@@ -39,36 +46,73 @@ for s in files:
     # Loop over the files in a sample
     for f in files[s]:
         if max_events > 0 and i >= max_events: break
+
         reader = Reader(f)
+        reader.collections = ["MCParticle", "PandoraPFOs"]#, "SiTracks_Refitted"]
+        print(type(reader))
+        print(dir(reader))
+
 
         # Loop over events in each file
         for event in reader.get('events'):
             if max_events > 0 and i >= max_events: break
             if i%100 == 0: print("\tProcessing event:", i)
 
+            # Make counters and key objects to track
+            n_pfo = 0
+            n_mcp = 0
+            n_pfo_el = 0
+            n_mcp_el = 0
+            n_matched_el = 0
+            my_mcp_el = None
+
             # Get the collections we care about
             mcps = event.get("MCParticle")
             pfos = event.get("PandoraPFOs")
-            trks = event.get('SiTracks_Refitted')
+            trks = event.get("SiTracks_Refitted")
+            #lnks = event.get("MCParticle_SiTracks_Refitted")
 
-            # Loop over MCPs
+            ######## Loop over MCPs
+
             for mcp in mcps:
                 if not mcp.getGeneratorStatus() == 1: continue
                 mcp_tlv = getTLV(mcp)
                 fillObjHists(hists[s], "mcp", mcp_tlv)
+                n_mcp += 1
 
                 # Look at electrons only
                 if abs(mcp.getPDG()) == 11:
                     fillObjHists(hists[s], "mcp_el", mcp_tlv)
+                    my_mcp_el = mcp_tlv
+                    n_mcp_el += 1
 
-            # Loop over PFOs
+            hists[s]["mcp_n"].Fill(n_mcp)
+            hists[s]["mcp_el_n"].Fill(n_mcp_el)
+
+            ######## Loop over PFOs
+
             for pfo in pfos:
                 pfo_tlv = getTLV(pfo)
                 fillObjHists(hists[s], "pfo", pfo_tlv)
+                n_pfo += 1
 
                 # Look at electrons only
                 if abs(pfo.getType()) == 11:
                     fillObjHists(hists[s], "pfo_el", pfo_tlv)
+                    n_pfo_el += 1
+
+                    # Look at electrons matched to the gun electron
+                    if isMatched(pfo_tlv, my_mcp_el):
+                        fillObjHists(hists[s], "mcp_el_match", my_mcp_el)
+                        n_matched_el += 1
+
+            hists[s]["pfo_n"].Fill(n_pfo)
+            hists[s]["pfo_el_n"].Fill(n_pfo_el)
+            hists[s]["mcp_el_match_n"].Fill(n_matched_el)
+
+            ######## Loop over tracks
+
+            #for trk in trks:
 
             # Iterate counter
             i += 1
@@ -76,16 +120,17 @@ for s in files:
 # Draw all the 1D histograms you filled
 for i, h in enumerate(hists[s]):
 
-    c = ROOT.TCanvas("c%i"%i, "c%i"%i)
+    # Collect hists that go on a single plot
+    hists_to_plot = {}
     for j, s in enumerate(hists):
-        if j==0:
-            hists[s][h].Draw()
-            hists[s][h].GetXaxis().SetTitle(h)
-            hists[s][h].GetYaxis().SetTitle("Entries")
-        else:
-            hists[s][h].Draw("same")
-        hists[s][h].SetLineColor(colors[j])
-        hists[s][h].SetMarkerColor(colors[j])
+        hists_to_plot[s] = hists[s][h]
+    var_name = h.split("_")[-1]
+    try:
+        xlabel = variables["obj"][var_name]["label"]
+    except:
+        xlabel = variables["evt"][var_name]["label"]
 
-    c.BuildLegend()
-    c.SaveAs("plots/electrons/"+h+".png")
+    # Call plotting function
+    plotHistograms(hists_to_plot, "plots/electrons/"+h+".png", xlabel, "Entries")
+    plotHistograms(hists_to_plot, "plots/electrons/"+h+".root", xlabel, "Entries")
+
--- plotHelper.py
+++ plotHelper.py
@@ -1,11 +1,11 @@
 
 print("Loading in plotHelper.py")
 
-variables = {"obj": {}, "event": {}}
-variables["obj"]["pt"] =  {"nbins": 20, "xmin": 0,     "xmax": 2000,    "accessor": ".Perp()"}
-variables["obj"]["eta"] = {"nbins": 20, "xmin": -3,    "xmax": 3,       "accessor": ".Eta()"}
-variables["obj"]["phi"] = {"nbins": 20, "xmin": -3.5,  "xmax": 3.5,     "accessor": ".Phi()"}
-variables["event"]["n"] = {"nbins": 20, "xmin": 0,     "xmax": 20,      "accessor": ""}
+variables = {"obj": {}, "evt": {}}
+variables["obj"]["pt"] =  {"nbins": 20, "xmin": 0,     "xmax": 2000,    "accessor": ".Perp()",  "label": "p_{T} [GeV]"}
+variables["obj"]["eta"] = {"nbins": 20, "xmin": -3,    "xmax": 3,       "accessor": ".Eta()",   "label": "#eta"}
+variables["obj"]["phi"] = {"nbins": 20, "xmin": -3.5,  "xmax": 3.5,     "accessor": ".Phi()",   "label": "#phi"}
+variables["evt"]["n"] =   {"nbins": 20, "xmin": 0,     "xmax": 20,      "accessor": "",         "label": "number per event"}
 
 colors =        [ROOT.TColor.GetColor("#FFA900"),   # Sunny Orange
                 ROOT.TColor.GetColor("#3629AC"),    # Dark Blue
@@ -13,8 +13,6 @@ colors =        [ROOT.TColor.GetColor("#FFA900"),   # Sunny Orange
                 ROOT.TColor.GetColor("#F65866"),    # Pink
                 ROOT.TColor.GetColor("#0E81C4"),    # Light Blue
                 ]
-
-
 
 def getTLV(obj):
     obj_p = obj.getMomentum()
@@ -26,3 +24,66 @@ def getTLV(obj):
 def fillObjHists(hists, objtype, obj):
     for var in variables["obj"]:
         hists[objtype+"_"+var].Fill(eval("obj"+variables["obj"][var]["accessor"]))
+
+def getMaximum(hists, isLog=False):
+    max_factor = 1.1
+    if isLog: max_factor = 10
+    maximum = max(hists[i].GetMaximum() for i in range(len(hists)))
+    return maximum*max_factor
+
+def colorHists(hists):
+    i=0
+    for h in hists:
+        h.SetLineColor(colors[i])
+        h.SetMarkerColor(colors[i])
+        i += 1
+        i %= len(colors)
+    return
+
+
+# From a map of histograms (key as label name), plot them all on a canvas and save
+def plotHistograms(h_map, save_name, xlabel="", ylabel="", interactive=False, logy=False, atltext=""):
+
+    can = ROOT.TCanvas("can", "can")
+    h_keys = list(h_map.keys())
+    h_values = list(h_map.values())
+
+    if len(h_keys)<1:
+        print("No histograms found in h_map. Drawing blank canvas.")
+        return
+
+    # Get maxes/mins of all hists
+    maxy = 1.5*getMaximum(h_values)
+    miny = 0
+    if logy:
+        maxy*=1e4
+        miny = 1e-1
+
+    # Draw histograms
+    colorHists(h_values)
+    h_map[h_keys[0]].GetXaxis().SetTitle(xlabel)
+    h_map[h_keys[0]].GetYaxis().SetTitle(ylabel)
+    h_values[0].SetMinimum(miny)
+    h_values[0].SetMaximum(maxy)
+    h_map[h_keys[0]].Draw("hist")
+    if logy: ROOT.gPad.SetLogy(1)
+    for k in h_keys[1:]:
+        h_map[k].Draw("hist same")
+
+    leg = ROOT.TLegend(.66, .64, .9, .88)
+    for k in h_keys:
+        leg.AddEntry(h_map[k], k, "l")
+    leg.Draw()
+    if interactive: raw_input("...")
+
+    if atltext != "":
+        ROOT.ATLASLabel(0.2,0.85, atltext[0])
+        text = ROOT.TLatex()
+        text.SetNDC()
+        text.SetTextSize(0.04)
+        for i, t in enumerate(atltext[1:]):
+            text.DrawLatex(0.2,0.85-0.07*(i+1), t)
+
+    can.SaveAs(save_name)
+    return
+
