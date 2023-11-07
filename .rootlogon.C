91aae30a5f521cf78b92f798c6725a4e205ead26
Tova Holmes <tova.r.holmes@gmail.com>
2023-11-07 08:13:55 -0600

    Included my root style setter.

---
+++ .rootlogon.C
@@ -0,0 +1,7 @@
+{
+    printf("Running rootlogon.C\n");
+    //gROOT->LoadMacro("/home/trholmes/utils/tdrstyle.C"); 
+    //setTDRStyle();
+    gROOT->LoadMacro("/home/trholmes/utils/AtlasStyle.C");
+    SetAtlasStyle();
+}
